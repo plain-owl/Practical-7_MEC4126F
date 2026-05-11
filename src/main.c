94a5e227db3af70c3dab03c0903aeff4da438575
Maruva Marimba <mrmmar010@myuct.ac.za>
2026-05-11 23:12:31 +0200

    Q

--- src/main.c
+++ src/main.c
@@ -70,8 +70,7 @@ void init_ADC(void){
     GPIOA->MODER &= ~(GPIO_MODER_MODER5|GPIO_MODER_MODER6); // reset pins 5 and 6
     GPIOA->MODER |= (GPIO_MODER_MODER5|GPIO_MODER_MODER6); // set pins 5 and 6 to analogue mode
 
-    ADC1->CHSELR |= ADC_CHSELR_CHSEL5; // select channel 5, connected to PA5
-    ADC1->CHSELR |= ADC_CHSELR_CHSEL6; //select channel 6, connected to PA6
+    ADC1->CHSELR |= (ADC_CHSELR_CHSEL5|ADC_CHSELR_CHSEL6); // select channel 5, connected to PA5 and select channel 6, connected to PA6
     ADC1->CFGR1 |= ADC_CFGR1_CONT; // set ADC to continuous mode
     ADC1->CFGR1 &= ~ADC_CFGR1_RES; // set ADC resolution to 12 bit,  0.088 degrees per step approximately (assuming about 360 degrees full rotation of pot)
     ADC1->CFGR1 |= ADC_CFGR1_WAIT;
@@ -138,12 +137,19 @@ float PI_control(float command, float feedback){
 void TIM6_IRQHandler(void){
     if (TIM6->SR & TIM_SR_UIF) { // check flag
         TIM6->SR &= ~TIM_SR_UIF; // clear flag
-        float position_des = ADC1->DR;
-        float position_act = ADC1->DR;
-        float voltage_req = PI_control(position_des, position_act); // required control action as a voltage 
+        uint16_t channel5_reading, channel6_reading; 
+        float position_des, position_act, voltage_req;
+        while (!(ADC1->ISR & ADC_ISR_EOC));
+        channel5_reading = ADC1->DR;
+        while (!(ADC1->ISR & ADC_ISR_EOC)); // risky because what's the order
+        channel6_reading = ADC1->DR;
+        position_des = (channel5_reading / 4096) * 3.3; // converts the ADC value read from channel 5 (PA5) into the control voltage
+        position_act = (channel6_reading / 4096) * 3.3 ; // converts the ADC value read from channel 6 (PA6) into the feedback voltage 
+        voltage_req = PI_control(position_des, position_act); // required control action as a voltage 
         TIM3->CCR1 = (uint16_t)((voltage_req + max_command/(min_command + max_command)) * TIM3->ARR); // converts the output of PI_control to a number between 0 - 1 (duty) then to a value for ccr1 
+    }
         
-}
+} 
 
 //********************************************************************
 // END OF PROGRAM
